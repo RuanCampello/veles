7d71ec251a154298b8c56cec8df85bc018c33ec8
RuanCampello <ruancampello999@gmail.com>
2025-10-07 08:26:13 -0300

    feat: basic database setup

---
+++ internal/db.go
@@ -0,0 +1,42 @@
+package internal
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"log"
+
+	"github.com/uptrace/bun"
+	"github.com/uptrace/bun/dialect/sqlitedialect"
+)
+
+type Db struct {
+	*bun.DB
+	ctx context.Context
+}
+
+type Habit struct {
+	bun.BaseModel `bun:"table:habits"`
+
+	Id   int64  `bun:"id, pk, autoincrement"`
+	Name string `bun:"name, notnull, unique"`
+}
+
+func New(ctx context.Context) (*Db, error) {
+	sql, err := sql.Open("sqlite", "veles.db")
+	if err != nil {
+		return nil, fmt.Errorf("Failed to open database: %w", err)
+	}
+
+	db := bun.NewDB(sql, sqlitedialect.New())
+	if err = createTables(ctx, db); err != nil {
+		return nil, err
+	}
+
+	log.Println("database initialised successfully")
+	return &Db{db, ctx}, nil
+}
+
+func createTables(ctx context.Context, db *bun.DB) error {
+	panic("createTables not implemented")
+}
